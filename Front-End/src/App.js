ad6d22853556132fdc5acde0588ebb1e2d42ff28
kijan <mohamedkijan321@gmail.com>
2024-12-30 00:28:41 +0530

    updared all the my task management and service management ui Api in App.js filw with protectedRoutes - for ensure user shoudl authorized or login to this system after then only they can aacess the appplication.

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -23,37 +23,36 @@ import EmployeeManageTask from './page/employee/EmployeeManageTask';
 import EmployeeAddTaskProgress from './page/employee/EmployeeAddTaskProgress';
 
 
+//to protect the routes
+import ProtectedRoute from './Routes/ProtectedRoute';
+
 function App() {
   return (
     <Router>
       <Routes>
         {/* Main Routes */}
-
         <Route path="/" element={< MainPage/>} />
-
         <Route path="/login" element={<SigninPage/>} />
         
-
         {/* Admin-Side Routes */}
-        <Route path="/admin-Dashboard" element={<AdminDashboard/>} />
-        <Route path="/adminChange-password" element={<AdminChangePassword />} />
+        <Route path="/admin-Dashboard" element={<ProtectedRoute><AdminDashboard/></ProtectedRoute>} />
+        <Route path="/adminChange-password" element={<ProtectedRoute><AdminChangePassword /></ProtectedRoute>} />
 
         {/* Task - Routes */}
-        <Route path="/admin-manage-task" element={<AdminManageTask/>}/>
-        <Route path="/admin-add-task" element={<AdminAddTask/>}/>
-        <Route path="/admin-recived-task" element={<AdminReceivedTask/>}/>
-
+        <Route path="/admin-manage-task" element={<ProtectedRoute><AdminManageTask/></ProtectedRoute>}/>
+        <Route path="/admin-add-task" element={<ProtectedRoute><AdminAddTask/></ProtectedRoute>}/>
+        <Route path="/admin-recived-task" element={<ProtectedRoute><AdminReceivedTask/></ProtectedRoute>}/>
         {/* Service - Routes */}
-        <Route path="/admin-add-service" element={<AdminAddService/>}/>
+        <Route path="/admin-add-service" element={<ProtectedRoute><AdminAddService/></ProtectedRoute>}/>
 
 
         {/* Employee-Side Routes */}
         <Route path="/employee-dashboard" element={<EmployeeDashboard/>}/>
 
         {/* Task - Routes */}
-        <Route path="/employee-progress-task" element={<EmployeeAddTaskProgress/>}/>
-        <Route path="/employee-recived-task" element={<EmployeeReceivedTask/>}/>
-        <Route path="/employee-manage-task-prgress" element={<EmployeeManageTask/>}/>
+        <Route path="/employee-progress-task" element={<ProtectedRoute><EmployeeAddTaskProgress/></ProtectedRoute>}/>
+        <Route path="/employee-recived-task" element={<ProtectedRoute><EmployeeReceivedTask/></ProtectedRoute>}/>
+        <Route path="/employee-manage-task-prgress" element={<ProtectedRoute><EmployeeManageTask/></ProtectedRoute>}/>
 
       </Routes>
     </Router>
