0ece50d6b41139fd11e329d98df5e52913da1427
Sathyajith <Sathyajith@gmail.com>
2024-12-29 11:44:59 +0530

    changed title and icon

--- Front-End/src/page/aboutUs.js
+++ Front-End/src/page/aboutUs.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/aboutus.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Navbar from '../components/templetes/adminNavBar';
+import Navbar from '../components/templetes/MainNav';
 import Footer from "../components/templetes/Footer";
 
 // Import images
