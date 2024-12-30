a6e9e3c45c9ef63f2ca8ae3cb8a5267c1142e0a7
Shaili Manamperi <119599080+ShailiManamperi@users.noreply.github.com>
2024-12-30 18:07:21 +0530

    Update ESideBar.js

--- Front-End/src/components/templetes/ESideBar.js
+++ Front-End/src/components/templetes/ESideBar.js
@@ -13,7 +13,7 @@ const Sidebar = ({ sidebarVisible }) => {
       <ul>
         <li><a href="#attendance"><img src={attendence} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Attendance</a></li>
         <li><a href="#invoice"><img src={invoice} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Invoice</a></li>
-        <li><a href="#payment"><img src={payment} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Payment</a></li>
+        <li><a href="/employee-payment"><img src={payment} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Payment</a></li>
         <li><a href="#task"><img src={task} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Task</a></li>
         <li><a href="#mailbox"><img src={mail} alt="Attendance" style={{ width: '20px', marginRight: '14px' }} /> Mail-Box</a></li>
       </ul>
