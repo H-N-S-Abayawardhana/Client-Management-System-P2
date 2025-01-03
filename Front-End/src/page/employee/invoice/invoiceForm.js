fef416bedd2ace99a4ee7c3b50de0d1562d96956
Shaili Manamperi <119599080+ShailiManamperi@users.noreply.github.com>
2025-01-03 10:37:59 +0530

    Update invoiceForm.js

--- Front-End/src/page/employee/invoice/invoiceForm.js
+++ Front-End/src/page/employee/invoice/invoiceForm.js
@@ -7,7 +7,7 @@ import "../../../css/employee/invoice/invoiceForm.css";
 import axios from "axios";
 import Navbar from "../../../components/templetes/empNavBar";
 import Sidebar from "../../../components/templetes/ESideBar";
-import Footer from "../../../components/templetes/Footer";
+import Footer from "../../../components/PagesFooter";
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 
