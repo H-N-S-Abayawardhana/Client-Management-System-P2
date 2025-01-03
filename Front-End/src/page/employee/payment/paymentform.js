6285b35bc3bc020fcdf29e05f8fdcf31bd654007
Shaili Manamperi <119599080+ShailiManamperi@users.noreply.github.com>
2025-01-03 10:38:36 +0530

    Update paymentform.js

--- Front-End/src/page/employee/payment/paymentform.js
+++ Front-End/src/page/employee/payment/paymentform.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../../components/templetes/empNavBar";
 import Sidebar from "../../../components/templetes/ESideBar";
-import Footer from "../../../components/templetes/Footer";
+import Footer from "../../../components/PagesFooter";
 import axios from "axios";
 import "../../../css/employee/payment/payment_method.css";
 import cvvImage from '../../../assets/cvv.png';
