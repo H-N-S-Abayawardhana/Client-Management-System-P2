194a661e6334f9462f9da740ddc2281e0dcb95b0
Niwarthana <147362189+H-N-S-Abayawardhana@users.noreply.github.com>
2025-01-16 20:42:36 +0530

    Update signinPage.js

--- Front-End/src/page/signinPage.js
+++ Front-End/src/page/signinPage.js
@@ -47,7 +47,7 @@ function Signin() {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://client-management-system-p2-gzspdw.fly.dev/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
