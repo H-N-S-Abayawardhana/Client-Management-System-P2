2ff26bd5a7b6f23a6e29f2e73a454bec089b61df
Sathyajith <Sathyajith@gmail.com>
2025-01-24 15:53:16 +0530

    backend updated

--- Back-End/index.js
+++ Back-End/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
-  origin: ['https://gamage-recruiters-cms.netlify.app'], // No trailing slash
+  origin: ['https://gamage-recruiters-cms.vercel.app'], // No trailing slash
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS for preflight requests
   credentials: true, // Allow credentials like cookies
 }));
